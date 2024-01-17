2e4c06dbf1e0c9bfecd71a09bc8804ca4f4a489e
Thomas Fini Hansen <xen@xen.dk>
2024-01-17 21:56:15 +0100

    Reorganize tests

--- tests/test-running.el
+++ tests/test-running.el
@@ -4,6 +4,42 @@
 (require 'custode)
 
 (describe "running tasks"
+  (describe "custode-buffer-name"
+    (it "should generate an adequate buffor name"
+      (expect (custode-buffer-name "project" "task")
+              :to-equal
+              " *custode project task*")))
+
+  (describe "custode--trigger"
+    :var (custode--tasks custode--task-states)
+    (before-each
+      (setq custode--tasks '())
+      (setq custode--task-states '())
+      (spy-on 'custode--current-project-root :and-return-value "unrelated")
+      (custode-add-task "task" "unrelated command")
+      (custode-enable-task "task")
+      (spy-on 'custode--current-project-root :and-return-value "project")
+      (custode-add-task "task" "the command")
+      (custode-enable-task "task")
+      (spy-on 'custode--start))
+
+    (it "triggers task running"
+      (custode--trigger "project")
+      (expect 'custode--start :to-have-been-called-with "project" "task" "the command"))
+
+    (it "triggers multiple task running"
+      (custode-add-task "task2" "the other command")
+      (custode-enable-task "task2")
+      (custode--trigger "project")
+      (expect 'custode--start :to-have-been-called-with "project" "task" "the command")
+      (expect 'custode--start :to-have-been-called-with "project" "task2" "the other command"))
+
+    (it "triggers task with args"
+      (custode-set-task-args "task" "the arguments")
+      (custode--trigger "project")
+      (expect 'custode--start :to-have-been-called-with "project" "task" "the command the arguments")
+      ))
+
   (describe "custode--start"
     (it "runs task in background"
       (assess-with-preserved-buffer-list
@@ -71,40 +107,4 @@
          (with-current-buffer (get-buffer (custode-buffer-name "project" "task"))
            (expect (window-point (get-buffer-window (current-buffer)))
                    :to-equal
-                   (point-max)))))))
-
-  (describe "custode--trigger"
-    :var (custode--tasks custode--task-states)
-    (before-each
-      (setq custode--tasks '())
-      (setq custode--task-states '())
-      (spy-on 'custode--current-project-root :and-return-value "unrelated")
-      (custode-add-task "task" "unrelated command")
-      (custode-enable-task "task")
-      (spy-on 'custode--current-project-root :and-return-value "project")
-      (custode-add-task "task" "the command")
-      (custode-enable-task "task")
-      (spy-on 'custode--start))
-
-    (it "triggers task running"
-      (custode--trigger "project")
-      (expect 'custode--start :to-have-been-called-with "project" "task" "the command"))
-
-    (it "triggers multiple task running"
-      (custode-add-task "task2" "the other command")
-      (custode-enable-task "task2")
-      (custode--trigger "project")
-      (expect 'custode--start :to-have-been-called-with "project" "task" "the command")
-      (expect 'custode--start :to-have-been-called-with "project" "task2" "the other command"))
-
-    (it "triggers task with args"
-      (custode-set-task-args "task" "the arguments")
-      (custode--trigger "project")
-      (expect 'custode--start :to-have-been-called-with "project" "task" "the command the arguments")
-      ))
-
-  (describe "custode-buffer-name"
-    (it "should generate an adequate buffor name"
-      (expect (custode-buffer-name "project" "task")
-              :to-equal
-              " *custode project task*"))))
+                   (point-max))))))))
